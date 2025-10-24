af6a3331510551241c24cddf41d35e36175a4958
DreBarker <Udemezueanthony9@gmail.com>
2025-10-24 10:59:50 +0100

    fix favicon

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,9 @@ import MainSidebarContextProvider from "@/store/main-sidebar-context";
 export const metadata = {
   title: "Webstack-ICT-Global",
   description: "Africa's Premier Tech Hub",
+  icons: {
+    icon: "/favicon.ico", // ✅ this restores the favicon
+  },
   openGraph: {
     images: [
       {
