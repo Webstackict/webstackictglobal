5845480b4eb432dbae50dfde2a676801ff6a8448
DreBarker <Udemezueanthony9@gmail.com>
2025-11-17 01:38:25 +0100

    fix image check

--- app/(public)/auth/email-verified/page.js
+++ app/(public)/auth/email-verified/page.js
@@ -4,6 +4,7 @@ import classes from "../page.module.css";
 import LinkWithProgress from "@/components/ui/Link-with-progress";
 import { iconsConfig } from "@/lib/icons/iconsConfig";
 import { motion, AnimatePresence } from "framer-motion";
+import checkIcon from "@/assets/successfull-check.svg";
 
 const MotionIcon = motion.create(iconsConfig["badgeCheck"]);
 export default async function VerifyEmail() {
@@ -14,17 +15,15 @@ export default async function VerifyEmail() {
           <div className={classes.glowBar}></div>
 
           <div id="login-header" className={classes.header}>
-            <AnimatePresence mode="wait">
-              <MotionIcon
-                key="check"
-                initial={{ scale: 0, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{
-                  type: "spring",
-                  duration: 1,
-                }}
-              />
-            </AnimatePresence>
+            <motion.img
+              src={checkIcon.src}
+              alt="check-icon"
+              initial={{ scale: 0, opacity: 0 }}
+              whileInView={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.5, type: "spring", bounce: 0.6 }}
+              viewport={{ once: true }}
+            />
+
             <h1>Email Verified Successfully</h1>
             <p>
               Your email has been verified, proceed to login and kickstart your
