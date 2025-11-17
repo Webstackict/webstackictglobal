dd04b68ed5089691cd760ce4c12285c52f9fe62e
DreBarker <Udemezueanthony9@gmail.com>
2025-11-17 01:26:36 +0100

    fix icon

--- app/(public)/auth/email-verified/page.js
+++ app/(public)/auth/email-verified/page.js
@@ -1,71 +1,29 @@
-"use server";
-
-// import { createSupabaseServerClient } from "@/lib/db/supabaseServer";
+"use client";
 import classes from "../page.module.css";
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+
 import LinkWithProgress from "@/components/ui/Link-with-progress";
-import ResendButton from "@/components/ui/resend-link-button";
-import AutoRefreshIfVerified from "@/components/poll/autorefreshVerification";
-import IconRenderer from "@/components/ui/icon-rederer";
+import { motion, AnimatePresence } from "framer-motion";
 
-const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-export default async function VerifyEmail({ searchParams }) {
-  const params = await searchParams;
-  const email = params.email;
-
-  const cookieStore = await cookies();
-
-  const supabase = createServerClient(supabaseURL, serviceRoleKey, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll();
-      },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          cookieStore.set(name, value, options);
-        });
-      },
-    },
-  });
-
-  const { data: userRaw, error: listError } =
-    await supabase.auth.admin.listUsers({
-      filter: `email=eq.${email}`,
-    });
-
-  const users = userRaw.users;
-
-  const user = users.find((u) => u.email === email);
-
-  if (!user) {
-    redirect("/auth");
-  }
-
-  const isEmailVerified = user.user_metadata.email_verified;
-
-  if (isEmailVerified === true) {
-    redirect("/auth");
-  }
-
-  console.log("sxcw", user);
-
-  const now = new Date();
-  const lastResend = user.confirmation_sent_at;
-  const waitResend = lastResend && now - new Date(lastResend) < 60 * 1000;
-
+const MotionIcon = motion.create("badgeCheck");
+export default async function VerifyEmail() {
   return (
     <section id="login-main" className={classes.authSection}>
-      <AutoRefreshIfVerified email={email} />
       <div className={classes.wrapper}>
         <div id="login-card" className={classes.card}>
           <div className={classes.glowBar}></div>
 
           <div id="login-header" className={classes.header}>
-            <IconRenderer label="verified-email" iconName="badgeCheck" />
+            <AnimatePresence mode="wait">
+              <MotionIcon
+                key="check"
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                  type: "spring",
+                  duration: 1,
+                }}
+              />
+            </AnimatePresence>
             <h1>Email Verified Successfully</h1>
             <p>
               Your email has been verified, proceed to login and kickstart your
